bf353316098a739319d3da0455e114c212e3df79
aqtagon <ahmed.danawi@gmail.com>
2024-01-17 20:40:39 -0800

    Added more to cateory routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,15 +27,49 @@ router.get('/:id', (req, res) => {
     Product
   ],
 })
-
+.then((dbCategoryData) => {
+  if (!dbCategoryData) {
+    res.status(404).json({ message: "No category found with this id" });
+    return;
+  }
+  res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  }
+  )
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: "No category found with this id" });
+      return;
+    }
 });
 
 router.delete('/:id', (req, res) => {
